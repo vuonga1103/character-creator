307f75711cf15e0ef9811fb1965fb493246dfab8
vuonga1103 <vuonga1103@yahoo.com>
2021-10-19 22:40:40 -0400

    Add perspective decoration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: 'fixed',
+          backgroundColor: 'hsl(195deg, 20%, 86%)',
+          height: '40%',
+          width: '100%',
+          top: '60%',
+          zIndex: -1,
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
